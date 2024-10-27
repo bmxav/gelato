0166fa977b70c88fb621e8e1f34061247f652d87
Bryan Xavier <5422569+bmxav@users.noreply.github.com>
2024-10-27 08:37:21 -0400

    Immutable bindings cannot be changed

--- crates/syntax/src/resolution.rs
+++ crates/syntax/src/resolution.rs
@@ -12,10 +12,23 @@ pub enum ResolutionError {
     InvalidAssignment,
     #[error("identifier has already been defined in this scope")]
     DuplicateDefinition(String),
+    #[error("assignment to immutable binding '{0}'")]
+    ImmutableAssignment(String),
+}
+
+pub struct Binding {
+    id: String,
+    mutable: bool,
+}
+
+impl Binding {
+    pub fn new(id: String, mutable: bool) -> Self {
+        Self { id, mutable }
+    }
 }
 
 pub struct Resolver {
-    scopes: Vec<HashMap<String, String>>,
+    scopes: Vec<HashMap<String, Binding>>,
     counter: usize,
 }
 
@@ -42,7 +55,7 @@ impl Resolver {
             Stmt::Import { .. } => stmt,
             Stmt::LetDecl { identifier, expr } => {
                 let resolved = self.resolve_expr(expr)?;
-                let id = self.new_id(&identifier)?;
+                let id = self.new_binding(&identifier, false)?;
                 Stmt::LetDecl {
                     identifier: id,
                     expr: resolved,
@@ -72,15 +85,24 @@ impl Resolver {
 
     fn resolve_expr(&mut self, expr: Expr) -> Result<Expr, ResolutionError> {
         let resolved = match expr {
-            Expr::Identifier(name) => Expr::Identifier(self.resolved_id(&name)?),
+            Expr::Identifier(ident) => Expr::Identifier(self.find_binding(&ident)?.id.clone()),
             Expr::BinaryExpr { op: BinaryOp::Assign, left, right } => {
-                let left = match *left {
-                    Expr::Identifier(_) => self.resolve_expr(*left)?,
+                let left_resolved = match *left {
+                    Expr::Identifier(ident) if !self.find_binding(&ident)?.mutable => {
+                        return Err(ResolutionError::ImmutableAssignment(ident.clone()))
+                    }
+                    Expr::Identifier(_) => {
+                        self.resolve_expr(*left)?
+                    }
                     _ => return Err(ResolutionError::InvalidAssignment)
                 };
 
-                let right = self.resolve_expr(*right)?;
-                Expr::BinaryExpr { op: BinaryOp::Assign, left: Box::new(left), right: Box::new(right) }
+                let right_resolved = self.resolve_expr(*right)?;
+                Expr::BinaryExpr {
+                    op: BinaryOp::Assign,
+                    left: Box::new(left_resolved),
+                    right: Box::new(right_resolved)
+                }
 
             }
             Expr::BinaryExpr { op, left, right } => {
@@ -105,7 +127,7 @@ impl Resolver {
         Ok(resolved)
     }
 
-    fn new_id(&mut self, identifier: &str) -> Result<String, ResolutionError> {
+    fn new_binding(&mut self, identifier: &str, mutable: bool) -> Result<String, ResolutionError> {
         self.counter += 1;
         let id = format!("{}.{}", identifier, self.counter);
 
@@ -114,21 +136,20 @@ impl Resolver {
                 Err(ResolutionError::DuplicateDefinition(identifier.to_string()))
             }
             Some(scope) => {
-                scope.insert(identifier.to_string(), id.clone());
+                scope.insert(identifier.to_string(), Binding::new(id.clone(), mutable));
                 Ok(id)
             }
             None => unreachable!("Scope should always be set"),
         }
     }
 
-    fn resolved_id(&self, identifier: &str) -> Result<String, ResolutionError> {
+    fn find_binding(&self, identifier: &str) -> Result<&Binding, ResolutionError> {
         for scope in self.scopes.iter().rev() {
-            match scope.get(identifier).map(|s| s.clone()) {
-                Some(id) => return Ok(id),
+            match scope.get(identifier) {
+                Some(binding) => return Ok(binding),
                 None => continue,
             }
         }
-
         Err(ResolutionError::UndefinedIdentifier(identifier.to_string()))
     }
 }
